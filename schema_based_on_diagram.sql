d934a3fc8d1b09b2174ec83f21bd9b671e780615
unknown <okorojiebube2@gmail.com>
2022-05-11 04:07:18 -0600

    Add foreign keys indexes

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,15 +1,15 @@
 CREATE TABLE IF NOT EXISTS patients (
 	id INT GENERATED ALWAYS AS IDENTITY,
 	name VARCHAR(300),
-  	date_of_birth DATE,
+  date_of_birth DATE,
 	PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS medical_histories(
 	id INT GENERATED ALWAYS AS IDENTITY,
 	admitted_at timestamp,
-  	patient_id int references patients(id),
-  	status varchar(300),
+  patient_id int references patients(id),
+  status varchar(300),
 	PRIMARY KEY (id)
 );
 
@@ -43,3 +43,12 @@ CREATE TABLE medical_documents (
   medical_histories_id INT references medical_histories(id), 
   treatments_id INT references treatments(id)
   );
+
+-- foreign keys indexes
+
+CREATE INDEX patient_id_asc ON medical_histories(patient_id ASC);
+CREATE INDEX medical_history_id_asc ON invoices(medical_history_id ASC);
+CREATE INDEX treatment_id_asc ON invoice_items(treatment_id ASC);
+CREATE INDEX invoice_id_asc ON invoice_items(invoice_id ASC);
+CREATE INDEX medical_histories_id_asc ON medical_documents(medical_histories_id ASC);
+CREATE INDEX treatments_id_asc ON medical_documents(treatments_id ASC);
